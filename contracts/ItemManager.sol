19305923c14191673c26b18187daab43f93fb5a9
Gaston Cisneros <gastoncisneros_97@hotmail.es>
2022-03-29 22:55:45 -0300

    Interacting with contracts via truffle console

--- contracts/ItemManager.sol
+++ contracts/ItemManager.sol
@@ -11,6 +11,7 @@ import "./Item.sol";
     struct S_Item{
         Item item;
         SupplyChainSteps step;
+        string stepDescription;
         string identifier;
         uint priceInWei;
     }
@@ -26,6 +27,7 @@ import "./Item.sol";
         items[index].identifier = identifier;
         items[index].priceInWei = itemPrice;
         items[index].step = SupplyChainSteps.Created;
+        items[index].stepDescription = "Creado";
         emit SupplyChainStep(index, identifier, uint(items[index].step), address(newItem));
     }
 
@@ -33,6 +35,7 @@ import "./Item.sol";
         require(items[itemIndex].priceInWei == msg.value, "Only full payments accepted");
         require(items[itemIndex].step == SupplyChainSteps.Created, "Item is further in the chain.");
         items[itemIndex].step = SupplyChainSteps.Paid;
+        items[index].stepDescription = "Pagado";
 
         emit SupplyChainStep(itemIndex,items[itemIndex].identifier, uint(items[itemIndex].step), address(items[itemIndex].item));
     }
@@ -40,6 +43,7 @@ import "./Item.sol";
     function triggerDelivery(uint itemIndex) public onlyOwner{
         require(items[itemIndex].step == SupplyChainSteps.Paid, "Item was not paid yet, is further in the chain.");
         items[itemIndex].step = SupplyChainSteps.Delivered;
+        items[index].stepDescription = "Entregado";
 
         emit SupplyChainStep(itemIndex,items[itemIndex].identifier, uint(items[itemIndex].step), address(items[itemIndex].item));
     }
